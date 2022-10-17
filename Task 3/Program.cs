07c8c66c3ca11fd240af45f3ff968632da1d9639
Andrei Savinkov <andrewsavinkov@gmail.com>
2022-10-17 15:56:22 +0500

    написал код

--- Task 3/Program.cs
+++ Task 3/Program.cs
@@ -4,3 +4,15 @@ m = 2, n = 3 -> A(m,n) = 9
 m = 3, n = 2 -> A(m,n) = 29
 */
 
+int Akkermann(int n, int m)
+{
+    if (n == 0) return m + 1;
+    if (n != 0 && m == 0) return Akkermann(n - 1, 1);
+    if (n > 0 && m > 0) return Akkermann(n - 1, Akkermann(n, m - 1));
+    return Akkermann(n, m);
+}
+
+int m = 3;
+int n = 2;
+int Ak=Akkermann(m, n);
+Console.WriteLine(Ak);
