5322a0fba232e7f63ef1ad9f8ae880540c3537a2
Andrei Savinkov <andrewsavinkov@gmail.com>
2022-10-17 10:20:43 +0500

    написал код

--- Task 1/Program.cs
+++ Task 1/Program.cs
@@ -5,3 +5,15 @@ N = 5 -> "5, 4, 3, 2, 1"
 N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
 */
 
+void ReturnIntegers(int n)
+{
+    if (n == 1)
+        Console.WriteLine(1);
+    else
+    {
+        Console.Write($"{n} \t");
+        ReturnIntegers(n - 1);
+    }
+}
+
+ReturnIntegers(100);
