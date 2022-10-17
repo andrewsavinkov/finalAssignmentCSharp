762a3af6d840c8efcc98f7796f0ac3a748d6ecbd
Andrei Savinkov <andrewsavinkov@gmail.com>
2022-10-17 15:38:49 +0500

    написал код

---
+++ Task 2/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+*/
+int sum = 0;
+int SumBetweenMN(int m, int n)
+{   
+    if (m<=n)
+    {
+        sum = sum + m;
+        SumBetweenMN(m+1, n);
+    }
+    return sum;
+}
+
+int m = 1;
+int n = 15;
+int total = SumBetweenMN(m, n);
+Console.WriteLine(total);
+
